89b926184f5786c069246a2aa3c5dca439de020a
Feargan <38695943+Feargan@users.noreply.github.com>
2022-12-27 16:36:14 +0100

    make disconnection reasons more clear

--- chat-server/app/websocket/endpoint.py
+++ chat-server/app/websocket/endpoint.py
@@ -1,8 +1,10 @@
 from dependency_injector.wiring import Provide, inject
 from fastapi import Depends
+from fastapi.encoders import jsonable_encoder
 from starlette import status
 from starlette.websockets import WebSocket
 
+from app.core.models.socket import SocketMessage, SocketOpcode
 from app.core.server.handler import ChatClientHandler
 from app.core.container import Container
 from app.core.security.auth import get_user, get_token_from_cookie, CredentialsError
@@ -16,17 +18,21 @@ async def websocket_endpoint(
     access_token: str | None = Depends(get_token_from_cookie),
     chat_service: ChatService = Depends(Provide[Container.chat_service]),
 ):
+    await websocket.accept()
+
     try:
         user = get_user(access_token)
     except CredentialsError as exc:
-        await websocket.close(code=status.HTTP_403_FORBIDDEN, reason=str(exc))
+        error_message = SocketMessage(opcode=SocketOpcode.error, data=str(exc))
+        await websocket.send_json(jsonable_encoder(error_message))
+        await websocket.close()
         return
 
     if chat_service.is_user_in_list(user):
-        await websocket.close(code=status.HTTP_403_FORBIDDEN, reason="Already in the room")
+        error_message = SocketMessage(opcode=SocketOpcode.error, data="Already in the room")
+        await websocket.send_json(jsonable_encoder(error_message))
+        await websocket.close()
         return
-
-    await websocket.accept()
 
     chat_client = WebSocketChatClient(user, websocket)
     chat_client_handler = ChatClientHandler(chat_service, chat_client)
