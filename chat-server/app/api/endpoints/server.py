ed7e82de7f37ad0dcb02a834e3a495703426f906
Feargan <38695943+Feargan@users.noreply.github.com>
2022-12-29 21:39:48 +0100

    pull server name from advertising settings

--- chat-server/app/api/endpoints/server.py
+++ chat-server/app/api/endpoints/server.py
@@ -1,7 +1,7 @@
 from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends
 
-from app.core.config import Settings
+from app.core.config import Settings, AdvertisingSettings
 from app.core.container import Container
 from app.core.models.info import ServerInfo
 from app.core.services.chat import ChatService
@@ -14,11 +14,12 @@ router = APIRouter(prefix="/server")
 @inject
 async def get_server_info(
         settings: Settings = Depends(Provide[Container.settings]),
+        advertising_settings: AdvertisingSettings = Depends(Provide[Container.advertising_settings]),
         chat_service: ChatService = Depends(Provide[Container.chat_service])
 ):
     num_clients = len(chat_service.get_user_list())
     return ServerInfo(
-        server_name=settings.SERVER_NAME,
+        server_name=advertising_settings.SERVER_NAME,
         num_clients=num_clients,
         max_clients=settings.MAX_CLIENTS,
     )
