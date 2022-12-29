4ba1ac1f110e56f5d5f2b7c017285a9384104892
Feargan <38695943+Feargan@users.noreply.github.com>
2022-12-29 21:59:31 +0100

    refactor: keep it simple, construct only valid objects

--- chat-server/app/core/exchanges/log.py
+++ chat-server/app/core/exchanges/log.py
@@ -9,32 +9,29 @@ from app.core.models.chat import ChatMessage
 
 
 class LogExchange:
-    def __init__(self, settings: Settings, channel: RobustChannel):
-        self.__exchange_name = settings.LOG_EXCHANGE_NAME
-        self.__exchange: AbstractRobustExchange | None = None
-        self.__queue_name = settings.LOG_QUEUE_NAME
-        self.__queue: AbstractRobustQueue | None = None
-        self.__channel = channel
-
-    async def setup(self):
-        self.__exchange = await self.__channel.declare_exchange(
-            name=self.__exchange_name,
-            type=ExchangeType.DIRECT,
-        )
-        self.__queue = await self.__channel.declare_queue(
-            name=self.__queue_name,
-            durable=True,
-        )
-        await self.__queue.bind(
-            exchange=self.__exchange,
-            routing_key=self.__queue_name,
-        )
+    def __init__(
+            self,
+            exchange: AbstractRobustExchange,
+            queue: AbstractRobustQueue
+    ):
+        self.__exchange = exchange
+        self.__queue = queue
 
     @staticmethod
     async def create(settings: Settings, channel: RobustChannel):
-        queue = LogExchange(settings, channel)
-        await queue.setup()
-        return queue
+        exchange = await channel.declare_exchange(
+            name=settings.LOG_EXCHANGE_NAME,
+            type=ExchangeType.DIRECT,
+        )
+        queue = await channel.declare_queue(
+            name=settings.LOG_QUEUE_NAME,
+            durable=True,
+        )
+        await queue.bind(
+            exchange=exchange,
+            routing_key=queue.name,
+        )
+        return LogExchange(exchange, queue)
 
     async def publish(self, chat_message: ChatMessage):
         serialized_message = json.dumps(jsonable_encoder(chat_message, exclude_none=True)).encode("utf-8")
@@ -45,5 +42,5 @@ class LogExchange:
                 content_type="application/json",
                 delivery_mode=DeliveryMode.PERSISTENT,
             ),
-            routing_key=self.__queue_name
+            routing_key=self.__queue.name
         )
