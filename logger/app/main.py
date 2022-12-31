76a66e3c8c392c5ba7e44b55d012a1431f3b7f2d
Feargan <38695943+Feargan@users.noreply.github.com>
2022-12-31 14:35:20 +0100

    log messages to db

--- logger/app/main.py
+++ logger/app/main.py
@@ -1,13 +1,44 @@
+import datetime
+import json
 import logging
 
 import pika
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
 
+from app.database import SessionLocal
+from app.models import Message, MessageType, QueuedMessage, InvalidMessage
 from settings import Settings
 
 
-def on_message_received(logger, channel, method, _, body):
-    # todo: save to db instead of printing
-    logger.info(f"Received: {body.decode()}")
+def on_message_received(logger: logging.Logger, database: Session, channel, method, _, body):
+    decoded_body = body.decode()
+    try:
+        deserialized_message = json.loads(decoded_body)
+        received_message = QueuedMessage(**deserialized_message)
+        message_to_save = Message(
+            sender_id=received_message.sender_id,
+            advertised_server=f"{received_message.advertised_address}:{received_message.advertised_port}",
+            type=MessageType(received_message.type),
+            text=received_message.text,
+            timestamp=datetime.datetime.fromisoformat(received_message.timestamp)
+        )
+        database.add(message_to_save)
+        database.commit()
+
+        text = f"text=\"{message_to_save.text}\", " if message_to_save.text is not None else ""
+        logger.info(f"Saved: {{"
+                    f"advertised_server=\"{message_to_save.advertised_server}\", "
+                    f"sender_id={message_to_save.sender_id}, type={message_to_save.type}, "
+                    f"{text} timestamp={message_to_save.timestamp}"
+                    f"}}")
+    except (ValueError, TypeError, IntegrityError) as exc:
+        logger.warning(f"Invalid message: {decoded_body}")
+        logger.exception(exc)
+        invalid_message = InvalidMessage(content=decoded_body)
+        database.rollback()
+        database.add(invalid_message)
+        database.commit()
     channel.basic_ack(delivery_tag=method.delivery_tag)
 
 
@@ -20,21 +51,28 @@ def main():
 
     logger.info(f"Connecting to {settings.BROKER_URL}")
 
-    connection = pika.BlockingConnection(pika.connection.URLParameters(settings.BROKER_URL))
-    channel = connection.channel()
+    broker = pika.BlockingConnection(pika.connection.URLParameters(settings.BROKER_URL))
+    channel = broker.channel()
 
     channel.queue_declare(queue=settings.LOG_QUEUE_NAME, durable=True)
 
     channel.basic_qos(prefetch_count=settings.QOS_PREFETCH_COUNT)
 
-    channel.basic_consume(
-        queue=settings.LOG_QUEUE_NAME,
-        on_message_callback=lambda ch, method, deliver, body: on_message_received(logger, ch, method, deliver, body)
-    )
+    database = SessionLocal()
 
-    logger.info(f"Consuming messages from {settings.LOG_QUEUE_NAME}")
+    try:
+        channel.basic_consume(
+            queue=settings.LOG_QUEUE_NAME,
+            on_message_callback=lambda ch, method, deliver, body:
+                on_message_received(logger, database, ch, method, deliver, body)
+        )
 
-    channel.start_consuming()
+        logger.info(f"Consuming messages from {settings.LOG_QUEUE_NAME}")
+
+        channel.start_consuming()
+    finally:
+        database.close()
+        broker.close()
 
 
 if __name__ == "__main__":
---
+++ logger/app/models.py
@@ -0,0 +1,60 @@
+import enum
+
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Enum
+
+from app.database import Base, engine
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    login = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+
+
+class MessageType(str, enum.Enum):
+    chat = "chat"
+    join = "join"
+    leave = "leave"
+
+
+class QueuedMessage:
+    def __init__(
+            self,
+            type: str,
+            timestamp: str,
+            advertised_address: str,
+            advertised_port: int,
+            text: str | None = None,
+            sender: str | None = None,
+            sender_id: int | None = None,
+    ):
+        self.type = type
+        self.timestamp = timestamp
+        self.advertised_address = advertised_address
+        self.advertised_port = advertised_port
+        self.text = text
+        self.sender = sender
+        self.sender_id = sender_id
+
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True, index=True)
+    advertised_server = Column(String, nullable=False)
+    sender_id = Column(Integer, ForeignKey("users.id"), nullable=True)
+    type = Column(Enum(MessageType), nullable=False)
+    text = Column(String)
+    timestamp = Column(DateTime, nullable=False)
+
+
+class InvalidMessage(Base):
+    __tablename__ = "invalid_messages"
+
+    id = Column(Integer, primary_key=True, index=True)
+    content = Column(String)
+
+
+Base.metadata.create_all(engine)
